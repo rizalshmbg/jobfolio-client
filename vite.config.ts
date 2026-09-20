45f2b9c308ff8ec701ff38ed01b3cb00cf2539fd
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-20 19:06:56 +0700

    feat: add path alias for @validations in tsconfig and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@pages': path.resolve(import.meta.dirname, './src/pages'),
       '@stores': path.resolve(import.meta.dirname, './src/stores'),
       '@/types': path.resolve(import.meta.dirname, './src/types'),
+      '@validations': path.resolve(import.meta.dirname, './src/validations'),
     },
   }
 });
