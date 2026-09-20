f6c7d82a3f6eb3a484c3b9ec4ad2796ed7566aa4
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-20 22:12:06 +0700

    feat: implement axios interceptors for request and response handling

---
+++ src/lib/axios-interceptors.ts
@@ -0,0 +1,52 @@
+import type { AxiosError, InternalAxiosRequestConfig } from 'axios';
+
+import { getAccessToken } from '@lib/auth-token';
+import { api } from '@lib/axios';
+import { refreshAccessTokenOnce } from '@lib/refresh-token';
+import { useAuthStore } from '@stores/auth.store';
+
+type RetryRequestConfig = InternalAxiosRequestConfig & {
+  _retry?: boolean;
+};
+
+export const setupAxiosInterceptors = () => {
+  // Request interceptor
+  api.interceptors.request.use((config) => {
+    const accessToken = getAccessToken();
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  });
+
+  // Response interceptor
+  api.interceptors.response.use(
+    (response) => response,
+
+    async (error: AxiosError) => {
+      const originalRequest = error.config as RetryRequestConfig | undefined;
+
+      if (
+        error.response?.status !== 401 ||
+        !originalRequest ||
+        originalRequest._retry
+      ) {
+        return Promise.reject(error);
+      }
+
+      originalRequest._retry = true;
+
+      try {
+        await refreshAccessTokenOnce();
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        useAuthStore.getState().setUnauthenticated();
+
+        return Promise.reject(refreshError);
+      }
+    },
+  );
+};
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,32 +1,10 @@
 import axios from 'axios';
 
-import { getAccessToken } from '@lib/auth-token';
-
-export const api = axios.create({
+const axiosConfig = {
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-});
+};
 
-// Add a request interceptor
-axios.interceptors.request.use((config) => {
-  const accessToken = getAccessToken();
+export const api = axios.create(axiosConfig);
 
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-
-  return config;
-});
-
-// Add a response interceptor
-// axios.interceptors.response.use((response) => {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     // Do something with response data
-//     return response;
-//   },
-//   function (error) {
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     // Do something with response error
-//     return Promise.reject(error);
-//   }
-// );
+export const refreshApi = axios.create(axiosConfig);
