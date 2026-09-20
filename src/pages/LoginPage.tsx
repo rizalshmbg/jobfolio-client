9cc5fbf94bf1ea46aef87d707313f6583a30eea5
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-20 20:37:01 +0700

    feat: implement login form with validation and mutation handling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,67 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router';
+
+import type { LoginInput } from '@validations/auth.validations';
+import { loginSchema } from '@validations/auth.validations';
+import { login } from '@api/auth.api';
+import { setAccessToken } from '@lib/auth-token';
+import { useAuthStore } from '@stores/auth.store';
+
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const setAuthenticated = useAuthStore((state) => state.setAuthenticated);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInput>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const loginMutation = useMutation({
+    mutationFn: login,
+    onSuccess: (response) => {
+      setAccessToken(response.data.accessToken);
+      setAuthenticated(response.data.user);
+      navigate('/dashboard', {
+        replace: true,
+      });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const onSubmit = (data: LoginInput) => {
+    loginMutation.mutate(data);
+  };
+
   return (
-    <h1>LoginPage</h1>
-  )
-}
-export default LoginPage
+    <main>
+      <h1>Login</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input id='email' type='text' {...register('email')} />
+          {errors.email && <p>{errors.email.message}</p>}
+        </div>
+
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input id='password' type='password' {...register('password')} />
+          {errors.password && <p>{errors.password.message}</p>}
+        </div>
+
+        <button type='submit' disabled={loginMutation.isPending}>
+          {loginMutation.isPending ? 'Logging in...' : 'Login'}
+        </button>
+      </form>
+    </main>
+  );
+};
+export default LoginPage;
