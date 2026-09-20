21cae088edd09939e5607a4055b8b93d57cb650a
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-20 17:51:03 +0700

    feat: add request interceptor to include access token in headers

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,32 @@
 import axios from 'axios';
 
+import { getAccessToken } from '@lib/auth-token';
+
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+// Add a request interceptor
+axios.interceptors.request.use((config) => {
+  const accessToken = getAccessToken();
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+// Add a response interceptor
+// axios.interceptors.response.use((response) => {
+//     // Any status code that lie within the range of 2xx cause this function to trigger
+//     // Do something with response data
+//     return response;
+//   },
+//   function (error) {
+//     // Any status codes that falls outside the range of 2xx cause this function to trigger
+//     // Do something with response error
+//     return Promise.reject(error);
+//   }
+// );
