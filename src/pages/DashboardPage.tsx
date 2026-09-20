8aca5824ba6404a0f04b6975a4562fe8a534721c
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-20 22:12:25 +0700

    feat: enhance DashboardPage to display user status and improve formatting

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,14 +1,18 @@
-import { useAuthStore } from "@stores/auth.store"
+import { useAuthStore } from '@stores/auth.store';
 
 const DashboardPage = () => {
   const user = useAuthStore((state) => state.user);
+  const status = useAuthStore((state) => state.status);
 
   return (
     <main>
       <h1>DashboardPage</h1>
 
-      {user && (<p>Welcome, {user.name}</p>)}
+      {user && <p>Welcome, {user.name}</p>}
+
+      <p>Status: {status}</p>
+      <p>User: {user?.name ?? 'No user'}</p>
     </main>
-  )
-}
-export default DashboardPage
+  );
+};
+export default DashboardPage;
