6bbf17e95d29c3357de512d05c84fdd513fd5ab2
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-20 17:38:38 +0700

    add path aliases for module resolution in tsconfig and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@lib': path.resolve(import.meta.dirname, './src/lib'),
+      '@pages': path.resolve(import.meta.dirname, './src/pages'),
+    },
+  }
+});
