8272306c19dbc636401484abcc89044f113b6166
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-21 19:32:20 +0700

    refactor: organize routes into GuestRoute and ProtectedRoute components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from 'react-router';
 
-import { useAuthInitialization } from '@hooks/use-auth-initialization';
-
 import ApplicationDetailPage from '@pages/ApplicationDetailPage';
 import ApplicationsPage from '@pages/ApplicationsPage';
 import DashboardPage from '@pages/DashboardPage';
@@ -9,21 +7,27 @@ import LandingPage from '@pages/LandingPage';
 import LoginPage from '@pages/LoginPage';
 import ProfilePage from '@pages/ProfilePage';
 import RegisterPage from '@pages/RegisterPage';
+import ProtectedRoute from '@routes/ProtectedRoute';
+import GuestRoute from '@routes/GuestRoute';
 
 const App = () => {
-  useAuthInitialization();
-
   return (
     <Routes>
-      <Route path='/' element={<LandingPage  />} />
+      <Route path='/' element={<LandingPage />} />
 
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
+      {/* Guest Routes */}
+      <Route element={<GuestRoute />}>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Route>
 
-      <Route path='/dashboard' element={<DashboardPage />} />
-      <Route path='/applications' element={<ApplicationsPage />} />
-      <Route path='/applications/:id' element={<ApplicationDetailPage />} />
-      <Route path='/profile' element={<ProfilePage />} />
+      {/* Protected Routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route path='/dashboard' element={<DashboardPage />} />
+        <Route path='/applications' element={<ApplicationsPage />} />
+        <Route path='/applications/:id' element={<ApplicationDetailPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Route>
     </Routes>
   );
 };
