27546cfc43fb50994d44233ab186ea2fc7360bdc
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-22 17:59:30 +0700

    refactor: remove StrictMode wrapper from main render

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { StrictMode } from 'react';
+// import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
@@ -11,11 +11,11 @@ import { setupAxiosInterceptors } from '@lib/axios';
 setupAxiosInterceptors();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </StrictMode>,
+  // </StrictMode>,
 );
