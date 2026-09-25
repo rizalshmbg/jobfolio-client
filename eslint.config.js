f93613ff967367cf7bad8a340d398413e02ed2af
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-25 19:33:44 +0700

    feat: disable react-hooks/set-state-in-effect rule for use-mobile.ts

--- eslint.config.js
+++ eslint.config.js
@@ -26,4 +26,10 @@ export default defineConfig([
       'react-refresh/only-export-components': 'off',
     },
   },
+  {
+    files: ['src/hooks/use-mobile.ts'],
+    rules: {
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
 ])
