8ed122db4a0e93c83d9c00923ca348e5975361ae
rizalshmbg <sihombingrizal18@gmail.com>
2026-09-26 18:56:36 +0700

    feat: enhance test setup with additional cleanup and toast dismissal

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,11 @@
 import '@testing-library/jest-dom/vitest';
 
+import { cleanup } from '@testing-library/react';
+import { toast } from 'sonner';
 import { afterAll, afterEach, beforeAll } from 'vitest';
 
+import { clearAccessToken } from '@lib/auth-token';
+import { useAuthStore } from '@stores/auth.store';
 import { server } from './mocks/server';
 
 beforeAll(() => {
@@ -11,7 +15,11 @@ beforeAll(() => {
 });
 
 afterEach(() => {
+  cleanup();
+  toast.dismiss();
   server.resetHandlers();
+  clearAccessToken();
+  useAuthStore.setState(useAuthStore.getInitialState());
 });
 
 afterAll(() => {
